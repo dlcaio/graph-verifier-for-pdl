08673a3f27aa4cd8516448e3d14486122dc9762a
Caio Della Libera <caiodellalibera@id.uff.br>
2019-11-10 18:09:32 -0300

    fecho aparentemente funcional, exceto quando eh necessario que ele seja nulo

--- graph-pdl-verifier.hs
+++ graph-pdl-verifier.hs
@@ -7,9 +7,9 @@ import Data.List (inits, tails)
 pdl = "beta;U(gama)(alfa;gama;bunta)"
 pdl2 = "alfa;gama;bunta"
 
-pdlFT = "*(alfa;beta)" -- -> (alfa;beta)
+pdlFT = "*(alfa);beta;*(alfa)" -- -> (alfa;beta)
 
-gFT = "alfa;beta;alfa;beta"
+gFT = "alfa;alfa;beta"
 
 pdl4 = "U(alfa;gama;bunta)(teta;U(gama;U(fi)(psi))(omega))"
 
@@ -55,6 +55,21 @@ splitChildren str = go 0 "" 0 str
                     else go newBalance (acc ++ [x]) times xs
             _ -> go (balance) (acc ++ [x]) times xs
 
+splitChildren1 :: String -> [String]
+splitChildren1 str = go 0 "" 0 str
+    where
+        go :: Int -> String -> Int -> String -> [String]
+        go _ acc 1 [] = []
+        go _ acc 1 s = [s]
+        go _ acc _ [] = []
+        go balance acc times (x:xs) = case x of
+            '(' -> go (balance + 1) (acc ++ [x]) (times) (xs)
+            ')' -> let newBalance = balance - 1 in
+                if newBalance == 0
+                    then (acc ++ [x]) : go 0 "" (times + 1) xs
+                    else go newBalance (acc ++ [x]) times xs
+            _ -> go (balance) (acc ++ [x]) times xs
+
 hd :: [a] -> [a]
 hd [] = []
 hd (a:as) = [a]
@@ -68,6 +83,12 @@ concatena a
     | length a >= 3 = [removeParenthesis (head a) ++ head (tail (tail a))]  ++ [removeParenthesis (head (tail a)) ++ head (tail (tail a))]
     | otherwise = [removeParenthesis (head a)]  ++ [removeParenthesis (head (tail a))]
 
+concatena1 :: [String] -> [String]
+concatena1 [] = []
+concatena1 a
+    | length a >= 2 = [removeParenthesis (head a)] ++ tail a
+    | otherwise = [removeParenthesis (head a)]
+ 
 nonDetChoice :: [String] -> String -> Bool
 nonDetChoice _ "" = False
 nonDetChoice p g
@@ -80,13 +101,35 @@ removeParenthesis a
     | hd a == "(" =  reverse(tail(reverse(tail a)))
     | otherwise = a
 
+countSemiColonPlusOne :: String -> Int
+countSemiColonPlusOne "" = 1
+countSemiColonPlusOne a
+    | hd a == ";" = 1 + countSemiColonPlusOne (tail a)
+    | otherwise = countSemiColonPlusOne (tail a)
 
+multiplica :: Int -> [String] -> String
+multiplica 0 pfch
+    | length pfch >= 2 = tail(head (tail pfch))
+    | otherwise = ""
+multiplica n pfch
+    | n == 1 && length pfch < 2 = (head pfch) ++ (multiplica (n - 1) pfch)
+    | otherwise = (head pfch) ++ ";" ++ (multiplica (n - 1) pfch)
+
+fecho :: Int -> [String] -> String -> Bool
+fecho (-1) pfch g = False
+--fecho 0 pfch g = verify (multiplica (0) (pfch)) (g)
+fecho n pfch g
+    | verify (multiplica (n) (pfch)) g == True = True
+    | otherwise = fecho (n - 1) (pfch) (g)
 
 verify :: String -> String -> Bool
 verify "" "" = True
 verify "" _ = False
 verify _ "" = False
 verify p g
+    | hd p == "*" = fecho (countSemiColonPlusOne g) (concatena1(splitChildren1 (tail p))) g
     | hd p == "U" = nonDetChoice (concatena (splitChildren (tail p))) (g) -- -> nonDetChoice ["(gama;U(omega)(teta))","(alfa;gama;bunta)"] graph
     | head (splitOn ";" p) == head (splitOn ";" g) = verify (tl(tail (splitOn ";" p))) (tl(tail (splitOn ";" g))) 
     | otherwise = False
+
+-- concatena1(splitChildren1(tail pdlFT))   ->     ["U(alfa)(beta)"]
